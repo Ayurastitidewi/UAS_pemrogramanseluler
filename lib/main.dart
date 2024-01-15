1a529da08de53e93603d3e18c1bd42254ea2adcc
Ayurastitidewi <155639396+Ayurastitidewi@users.noreply.github.com>
2024-01-15 17:55:41 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,99 +12,81 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(50),
-              bottomRight: Radius.circular(50),
-            ),
-          ),
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(50),
+                  bottomRight: Radius.circular(50))),
           toolbarHeight: 230, // default is 56
           toolbarOpacity: 0.9,
-          backgroundColor: Colors.deepPurple,
-          title: const Column(
+          title: Column(
             children: [
               Center(
-                child: CircleAvatar(
-                  radius: 50,
-                  backgroundImage: AssetImage('assets/images/me.jpeg'),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(9.0),
-                child: Text(
-                  "Sri Suartini",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
+                child: Container(
+                  child: CircleAvatar(
+                    radius: 50,
+                    backgroundImage: AssetImage('lib/assets/foto_dekayu.jpeg'),
                   ),
                 ),
               ),
-              Text(
-                "Student",
-                style: TextStyle(
-                  height: 1,
-                  fontSize: 14,
-                  color: Colors.white,
+              Padding(
+                padding: const EdgeInsets.all(9.0),
+                child: Text(
+                  "Ayu Rastiti Dewi",
+                  style: TextStyle(fontWeight: FontWeight.bold),
                 ),
-              )
+              ),
+              Text("Mahasiswa", style: TextStyle(height: 1, fontSize: 14))
             ],
           ),
         ),
         body: Center(
           child: Container(
             width: 400,
-            margin: const EdgeInsets.only(top: 35),
+            margin: EdgeInsets.only(top: 45),
             child: Column(
               children: [
-                const Text(
-                  "Tentang Saya",
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
+                Text("Tentang Saya", style: TextStyle(fontWeight: FontWeight.bold)),
                 Container(
-                  margin: const EdgeInsets.all(15),
-                  child: const Text(
-                    "Hai, nama saya Kadek Sri Suartini, saya tinggal di Batubulan dan saat ini saya sedang menempuh pendidikan di Universitas Pendidikan Nasional jurusan Teknologi Informasi",
-                    maxLines: 4,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(color: Colors.black),
-                  ),
-                ),
+                    margin: EdgeInsets.all(20),
+                    child: Text(
+                        "Haii, aku mamhasiswa Universitas Pendidikan Nasional jurusan Teknik Informasi angkatan 2021 kelas sore",
+                        maxLines: 4,
+                        textAlign: TextAlign.center,
+                        style:
+                            TextStyle(color: Color.fromARGB(0, 26, 25, 25).withOpacity(0.5)))),
                 Container(
                   width: 155,
-                  decoration: const BoxDecoration(
-                    border: Border(
-                      bottom: BorderSide(color: Colors.black, width: 0.1),
-                    ),
+                  decoration: BoxDecoration(
+                      border: Border(
+                          bottom: BorderSide(color: Colors.black, width: 0.1))),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text("Kemampuan",
+                        style: TextStyle(fontWeight: FontWeight.bold)),
                   ),
                 ),
                 Container(
-                  margin: const EdgeInsets.all(10),
-                  child: const Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Text(
-                      "Keahlian",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                ),
-                SizedBox(
+                    child: SizedBox(
                   width: 300,
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
                       Column(
                         children: [
-                          const Row(
+                          Row(
                             children: [
                               Text(
-                                "3",
+                                "1",
                                 style: TextStyle(fontWeight: FontWeight.bold),
                                 textAlign: TextAlign.center,
                               ),
                               Icon(
                                 Icons.star,
-                                color: Colors.orange,
-                                size: 18.0,
+                                color: Color.fromARGB(255, 255, 81, 0),
+                                size: 20.0,
                               ),
                             ],
                           ),
@@ -112,12 +94,9 @@ class MyApp extends StatelessWidget {
                             children: [
                               Padding(
                                 padding: const EdgeInsets.only(top: 5),
-                                child: Text(
-                                  "Web Design",
-                                  style: TextStyle(
-                                    color: Colors.black.withOpacity(0.5),
-                                  ),
-                                ),
+                                child: Text("HTML",
+                                    style: TextStyle(
+                                        color: Color.fromRGBO(231, 67, 17, 1).withOpacity(0.5))),
                               )
                             ],
                           )
@@ -125,16 +104,16 @@ class MyApp extends StatelessWidget {
                       ),
                       Column(
                         children: [
-                          const Row(
+                          Row(
                             children: [
                               Text(
-                                "4",
+                                "1",
                                 style: TextStyle(fontWeight: FontWeight.bold),
                                 textAlign: TextAlign.center,
                               ),
                               Icon(
                                 Icons.star,
-                                color: Colors.orange,
+                                color: Color.fromARGB(255, 243, 84, 10),
                                 size: 18.0,
                               ),
                             ],
@@ -143,12 +122,65 @@ class MyApp extends StatelessWidget {
                             children: [
                               Padding(
                                 padding: const EdgeInsets.only(top: 5),
-                                child: Text(
-                                  "Administrasi Keuangan",
-                                  style: TextStyle(
-                                    color: Colors.black.withOpacity(0.5),
-                                  ),
-                                ),
+                                child: Text("CSS",
+                                    style: TextStyle(
+                                        color: Color.fromARGB(255, 226, 77, 18).withOpacity(0.5))),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Row(
+                            children: [
+                              Text(
+                                "1",
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                                textAlign: TextAlign.center,
+                              ),
+                              Icon(
+                                Icons.star,
+                                color: Color.fromRGBO(255, 81, 0, 1),
+                                size: 18.0,
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(top: 5),
+                                child: Text("Figma",
+                                    style: TextStyle(
+                                        color: Color.fromARGB(255, 233, 90, 7).withOpacity(0.5))),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Row(
+                            children: [
+                              Text(
+                                "1",
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                                textAlign: TextAlign.center,
+                              ),
+                              Icon(
+                                Icons.star,
+                                color: Color.fromARGB(255, 255, 102, 0),
+                                size: 18.0,
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(top: 5),
+                                child: Text("Flutter",
+                                    style: TextStyle(
+                                        color: Color.fromRGBO(216, 78, 14, 1).withOpacity(0.5))),
                               )
                             ],
                           )
@@ -156,26 +188,24 @@ class MyApp extends StatelessWidget {
                       ),
                     ],
                   ),
-                ),
+                )),
                 Container(
-                  margin: const EdgeInsets.only(top: 10),
+                  margin: EdgeInsets.only(top: 10),
                   width: 155,
-                  decoration: const BoxDecoration(
-                    border: Border(
-                      bottom: BorderSide(color: Colors.black, width: 0.1),
-                    ),
-                  ),
+                  decoration: BoxDecoration(
+                      border: Border(
+                          bottom: BorderSide(color: Colors.black, width: 0.1))),
                 ),
                 Container(
-                  margin: const EdgeInsets.all(20),
-                  child: const Column(children: [
+                  margin: EdgeInsets.all(20),
+                  child: Column(children: [
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         Padding(
-                          padding: EdgeInsets.all(8.0),
+                          padding: const EdgeInsets.all(8.0),
                           child: Text(
-                            "Sosial media saya",
+                            "Kontak Ku",
                             style: TextStyle(fontWeight: FontWeight.bold),
                           ),
                         ),
@@ -187,7 +217,7 @@ class MyApp extends StatelessWidget {
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
                             Padding(
-                              padding: EdgeInsets.all(5.0),
+                              padding: const EdgeInsets.all(5.0),
                               child: Icon(
                                 Icons.facebook,
                                 color: Colors.blueAccent,
@@ -195,18 +225,18 @@ class MyApp extends StatelessWidget {
                               ),
                             ),
                             Padding(
-                              padding: EdgeInsets.all(5.0),
+                              padding: const EdgeInsets.all(5.0),
                               child: Icon(
-                                Icons.tiktok,
-                                color: Colors.blueAccent,
+                                Icons.telegram,
+                                color: Colors.lightBlue,
                                 size: 20.0,
                               ),
                             ),
                             Padding(
-                              padding: EdgeInsets.all(5.0),
+                              padding: const EdgeInsets.all(5.0),
                               child: Icon(
-                                Icons.telegram,
-                                color: Colors.blueAccent,
+                                Icons.email,
+                                color: Colors.blueGrey,
                                 size: 20.0,
                               ),
                             ),
